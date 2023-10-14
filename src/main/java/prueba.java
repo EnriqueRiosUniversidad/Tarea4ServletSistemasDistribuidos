36a9081c588a7e58ece4240b14353c2d5f161c36
Enrique Emmanuel Rios Chyrnia <enrique.rios2019@fiuni.edu.py>
2023-10-13 21:07:49 -0300

    subida del repositorio

---
+++ src/main/java/prueba.java
@@ -0,0 +1,58 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.client.RestTemplate;
+/**
+ * Servlet implementation class prueba
+ */
+@WebServlet("/prueba")
+public class prueba extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public prueba() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+
+		final String uriGetUsers= "https://jsonplaceholder.typicode.com/posts";
+		// https://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/web/client/RestTemplate.html
+		RestTemplate restTemplate = new RestTemplate();
+
+
+	
+		
+		String users= restTemplate.getForObject(uriGetUsers, String.class);
+		System.out.println(users);
+
+		
+		
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
